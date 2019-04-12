b368e8424679a692a75276860ff435075096105e
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-12 20:38:31 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {
+        
+      },
+      :matriarch => {
+        
+      },
+      :heroine => {
+        
+      },
+      :hero_friends => [
+        ]
       }, 
     :capulet => {
       #your key/value pairs here!
